52671ca3ab7d259c84df84247cc39996d408c7df
Jérémie Souques <jeremie.souques@safrangroup.com>
2024-08-19 17:50:52 +0200

    fix ProjectCreatorService

---
+++ BIA.ToolKit.Application/Settings/ProjectWithParam.cs
@@ -0,0 +1,28 @@
+﻿namespace BIA.ToolKit.Application.Settings
+{
+    public class ProjectWithParam
+    {
+        public bool WithFrontEnd { get; set; }
+
+        public bool WithFrontFeature { get; set; }
+
+        public bool WithServiceApi { get; set; }
+
+        public bool WithDeployDb { get; set; }
+
+        public bool WithWorkerService { get; set; }
+
+        public bool WithHangfire { get; set; }
+
+        public bool WithInfraData { get; set; }
+
+        public bool HasAllFeature =>
+            WithFrontEnd &&
+            WithFrontFeature &&
+            WithServiceApi &&
+            WithDeployDb &&
+            WithWorkerService &&
+            WithHangfire &&
+            WithInfraData;
+    }
+}
