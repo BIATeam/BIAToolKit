b3b77be69f9969759af3a7220986a07ca56652de
FOLIOT Kevin <kevin.foliot.ext@safrangroup.com>
2024-11-07 17:16:32 +0100

    Be able to handle the file moved or renamed when applying rejected file merge

--- BIA.ToolKit.Application/Services/GitService.cs
+++ BIA.ToolKit.Application/Services/GitService.cs
@@ -137,7 +137,7 @@
             outPut.AddMessageLine($"Apply diff", "Pink");
             outPut.AddMessageLine($"On project : {projectPath}", "Pink");
             // cd "...\\YourProject" git apply --reject --whitespace=fix "3.2.2-3.3.0.patch" \
-            int result = RunScript("git", $"apply --reject --whitespace=fix {migrateFilePath}", projectPath).Result;
+            int result = RunScript("git", $"apply --reject --unsafe-paths --whitespace=fix {migrateFilePath}", projectPath).Result;
             if (result == 0)
             {
                 outPut.AddMessageLine("Apply diff finished", actionFinishedAtEnd ? "Green" : "Blue");
@@ -168,86 +168,90 @@
             public string ProjectTargetPath { get; set; }
             public string ProjectTargetVersion { get; set; }
             public string ProjectPath { get; set; }
+            public string MigrationPatchFilePath { get; set; }
         }
 
         public void MergeRejeted(bool actionFinishedAtEnd, MergeParameter param)
         {
             outPut.AddMessageLine($"Apply merge on rejected", "Pink");
 
-            MergeRejetedDirectory(param.ProjectPath, param, param.ProjectPath);
+            MergeRejetedDirectory(param.ProjectPath, param);
 
             outPut.AddMessageLine("Apply merge on rejected", actionFinishedAtEnd ? "Green" : "Blue");
         }
 
         // Process all files in the directory passed in, recurse on any directories
         // that are found, and process the files they contain.
-        public void MergeRejetedDirectory(string targetDirectory, MergeParameter param, string projectPath)
+        public void MergeRejetedDirectory(string targetDirectory, MergeParameter param)
         {
             // Process the list of files found in the directory.
             string[] fileEntries = Directory.GetFiles(targetDirectory, "*.rej");
             foreach (string fileName in fileEntries)
-                MergeRejetedFileAsync(fileName, param, projectPath);
+                MergeRejetedFileAsync(fileName, param);
 
             // Recurse into subdirectories of this directory.
             string[] subdirectoryEntries = Directory.GetDirectories(targetDirectory);
             foreach (string subdirectory in subdirectoryEntries)
-                MergeRejetedDirectory(subdirectory, param, projectPath);
+                MergeRejetedDirectory(subdirectory, param);
         }
 
         // Insert logic for processing found files here.
-        public void MergeRejetedFileAsync(string path, MergeParameter param, string projectPath)
+        public void MergeRejetedFileAsync(string rejectedFilePath, MergeParameter param)
         {
-            outPut.AddMessageLine("Merge file '" + path + "'.", "White");
+            outPut.AddMessageLine("Merge file '" + rejectedFilePath + "'.", "White");
 
-            var diffInstruction = File.ReadAllLines(path).First();
-            (string originalFileRelativePath, string finalFileRelativePath) = ExtractOriginalAndFinalRelativePathOfDiffInstruction(diffInstruction);
+            var rejectedFileDiffInstruction = File.ReadAllLines(rejectedFilePath).First();
+            (string rejectedFileOriginalFileRelativePath, string rejectedFileTargetFileRelativePath) = ExtractOriginalAndFinalRelativePathOfDiffInstruction(rejectedFileDiffInstruction);
 
-            string finalFile = path.Substring(0, path.Length - 4);
-            string originalFile = param.ProjectOriginPath + finalFile.Substring(param.ProjectPath.Length);
-            string additionnalFile = param.ProjectTargetPath + finalFile.Substring(param.ProjectPath.Length);
+            var migrationPatchFileDiffInstruction = File.ReadLines(param.MigrationPatchFilePath).FirstOrDefault(l => l.EndsWith(rejectedFileTargetFileRelativePath));
+            (string migrationPatchFileOriginalFileRelativePath, string migrationPatchFileTargetFileRelativePath) = ExtractOriginalAndFinalRelativePathOfDiffInstruction(migrationPatchFileDiffInstruction);
 
-            if (!File.Exists(originalFile))
+            string finalProjectFile = rejectedFilePath.Substring(0, rejectedFilePath.Length - 4);
+            string originalProjectFile = Path.Combine(param.ProjectOriginPath, migrationPatchFileOriginalFileRelativePath.Replace("/", "\\"));
+            string targetProjectFile = Path.Combine(param.ProjectTargetPath, migrationPatchFileTargetFileRelativePath.Replace("/", "\\"));
+
+            if (!File.Exists(originalProjectFile))
             {
-                outPut.AddMessageLine($"Unable to perform merge : original file {originalFile} doesn't exist", "red");
+                outPut.AddMessageLine($"Unable to perform merge : original project's file {originalProjectFile} doesn't exist", "red");
                 return;
             }
 
-            if (!File.Exists(finalFile))
+            if (!File.Exists(targetProjectFile))
             {
-                outPut.AddMessageLine($"Unable to perform merge : final file {finalFile} doesn't exist", "red");
+                outPut.AddMessageLine($"Unable to perform merge : target project's file {targetProjectFile} doesn't exist", "red");
                 return;
             }
 
-            if (!File.Exists(additionnalFile))
+            if (!File.Exists(finalProjectFile))
             {
-                outPut.AddMessageLine($"Unable to perform merge : additionnal file {additionnalFile} doesn't exist", "red");
+                outPut.AddMessageLine($"Unable to perform merge : final project's file {finalProjectFile} doesn't exist", "red");
                 return;
             }
 
-            int result = RunScript("git", $"merge-file -L Src -L {param.ProjectOriginVersion} -L {param.ProjectTargetVersion} \"{finalFile}\" \"{originalFile}\" \"{additionnalFile}\"").Result;
+            int result = RunScript("git", $"merge-file -L Src -L {param.ProjectOriginVersion} -L {param.ProjectTargetVersion} \"{finalProjectFile}\" \"{originalProjectFile}\" \"{targetProjectFile}\"").Result;
             if (result == 0)
             {
-                File.Delete(path);
+                File.Delete(rejectedFilePath);
             }
             if (result > 0)
             {
-                outPut.AddMessageLine(result + " conflict to solve in file '" + path + "'.", "Yellow");
-                File.Delete(path);
+                outPut.AddMessageLine(result + " conflict to solve in file '" + rejectedFilePath + "'.", "Yellow");
+                File.Delete(rejectedFilePath);
             }
             else if (result < 0)
             {
-                outPut.AddMessageLine("Error " + result + " durring Merge file '" + path + "'.", "Red");
+                outPut.AddMessageLine("Error " + result + " durring Merge file '" + rejectedFilePath + "'.", "Red");
             }
         }
 
-        private (string OriginalRelativePath, string FinalRelativePath) ExtractOriginalAndFinalRelativePathOfDiffInstruction(string diffInstruction)
+        private (string OriginalRelativePath, string TargetRelativePath) ExtractOriginalAndFinalRelativePathOfDiffInstruction(string diffInstruction)
         {
             if (string.IsNullOrEmpty(diffInstruction))
             {
                 return (string.Empty, string.Empty);
             }
 
-            string pattern = @"a/(?<Part1>[^\s]+)\s+b/(?<Part2>.+)";
+            string pattern = @"a/(?<Part1>[^\s]+)\s+b/(?<Part2>[^\s]+)";
             var match = Regex.Match(diffInstruction, pattern);
 
             if (match.Success)
