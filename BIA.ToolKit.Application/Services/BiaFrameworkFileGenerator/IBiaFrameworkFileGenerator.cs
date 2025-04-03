3bd21faca57da897745af2d0a57f9827d3f5aa97
FOLIOT Kevin <kevin.foliot.ext@safrangroup.com>
2025-04-03 17:30:17 +0200

    Create and compare with BiaFrameworkVersion

--- BIA.ToolKit.Application/Services/BiaFrameworkFileGenerator/IBiaFrameworkFileGenerator.cs
+++ BIA.ToolKit.Application/Services/BiaFrameworkFileGenerator/IBiaFrameworkFileGenerator.cs
@@ -4,12 +4,13 @@
     using System.Collections.Generic;
     using System.Threading.Tasks;
     using BIA.ToolKit.Application.ViewModel;
+    using BIA.ToolKit.Common;
     using BIA.ToolKit.Domain.DtoGenerator;
     using BIA.ToolKit.Domain.ModifyProject;
 
     public interface IBiaFrameworkFileGenerator
     {
-        List<Version> CompatibleBiaFrameworkVersions { get; }
+        List<BiaFrameworkVersion> CompatibleBiaFrameworkVersions { get; }
         Task GenerateDto(Project project, EntityInfo entityInfo, string domainName, IEnumerable<MappingEntityProperty> mappingEntityProperties);
     }
 }
