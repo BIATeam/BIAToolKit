982ccc4049b986cec3358b6185345f7795038d5c
FOLIOT Kevin <kevin.foliot.ext@safrangroup.com>
2025-04-07 11:03:20 +0200

    Convert EntityDto template razor into scriban

--- BIA.ToolKit.Application/Services/BiaFrameworkFileGenerator/BiaFrameworkFileGeneratorService.cs
+++ BIA.ToolKit.Application/Services/BiaFrameworkFileGenerator/BiaFrameworkFileGeneratorService.cs
@@ -3,14 +3,17 @@
     using System;
     using System.Collections.Generic;
     using System.IO;
+    using System.Reflection;
     using System.Threading.Tasks;
     using BIA.ToolKit.Application.Helper;
+    using BIA.ToolKit.Application.Services.BiaFrameworkFileGenerator._4_0_0.Models;
     using BIA.ToolKit.Application.ViewModel;
     using BIA.ToolKit.Domain.DtoGenerator;
     using BIA.ToolKit.Domain.ModifyProject;
     using Microsoft.AspNetCore.Components;
     using Microsoft.AspNetCore.Components.Web;
     using Microsoft.Extensions.Logging;
+    using Scriban;
 
     public class BiaFrameworkFileGeneratorService
     {
@@ -36,10 +39,17 @@
 
         public async Task GenerateDto(Project project, EntityInfo entityInfo, string domainName, IEnumerable<MappingEntityProperty> mappingEntityProperties)
         {
-            await biaFrameworkFileGenerator.GenerateDto(project, entityInfo, domainName, mappingEntityProperties);
+            try
+            {
+                await biaFrameworkFileGenerator.GenerateDto(project, entityInfo, domainName, mappingEntityProperties);
+            }
+            catch(Exception ex)
+            {
+                consoleWriter.AddMessageLine($"ERROR: Fail to generate DTO : {ex}", color: "red");
+            }
         }
 
-        public async Task<string> GenerateFromTemplate(Type templateType, object model)
+        public async Task<string> GenerateFromTemplateWithRazor(Type templateType, object model)
         {
             using var htmlRenderer = new Microsoft.AspNetCore.Components.Web.HtmlRenderer(serviceProvider, loggerFactory);
             var html = await htmlRenderer.Dispatcher.InvokeAsync(async () =>
@@ -56,6 +66,12 @@
             });
 
             return html;
+        }
+
+        public static string GenerateFromTemplateWithScriban(object model)
+        {
+            var template = LoadTemplateFromEmbeddedResource(model);
+            return template.Render(model, member => member.Name);
         }
 
         public async Task GenerateFile(string content, string path)
@@ -76,6 +92,18 @@
             {
                 consoleWriter.AddMessageLine($"ERROR: Fail to generate file : {ex.Message}", color: "red");
             }
+        }
+
+        private static Template LoadTemplateFromEmbeddedResource(object model)
+        {
+            var modelType = model.GetType();
+            var templateResourcePath = modelType.FullName.Replace("Models", "Templates") + ".scriban";
+            var modelAssembly = modelType.Assembly;
+            using Stream stream = modelAssembly.GetManifestResourceStream(templateResourcePath)
+                ?? throw new FileNotFoundException(templateResourcePath);
+            using StreamReader reader = new(stream);
+            string templateText = reader.ReadToEnd();
+            return Template.Parse(templateText);
         }
     }
 }
--- BIA.ToolKit.Application/Services/BiaFrameworkFileGenerator/_4_0_0/BiaFrameworkFileGenerator_4_0_0.cs
+++ BIA.ToolKit.Application/Services/BiaFrameworkFileGenerator/_4_0_0/BiaFrameworkFileGenerator_4_0_0.cs
@@ -32,7 +32,7 @@
 
         public async Task GenerateDto(Project project, EntityInfo entityInfo, string domainName, IEnumerable<MappingEntityProperty> mappingEntityProperties)
         {
-            var model = new DtoModel
+            var dtoModel = new EntityDto
             {
                 CompanyName = project.CompanyName,
                 ProjectName = project.Name,
@@ -64,30 +64,62 @@
                 IsTeamType = entityInfo.BaseType?.Contains("Team") ?? false
             };
 
-            if (string.IsNullOrWhiteSpace(model.BaseKeyType))
+            var mapperModel = new DtoModel
+            {
+                CompanyName = project.CompanyName,
+                ProjectName = project.Name,
+                NameArticle = Common.ComputeNameArticle(entityInfo.Name),
+                DomainName = domainName,
+                DtoName = entityInfo.Name + "Dto",
+                EntityName = entityInfo.Name,
+                BaseKeyType = entityInfo.BaseKeyType,
+                Properties = mappingEntityProperties.Select(x => new PropertyModel()
+                {
+                    MappingName = x.MappingName,
+                    EntityCompositeName = x.EntityCompositeName,
+                    MappingType = x.MappingType,
+                    MappingDateType = x.MappingDateType,
+                    IsOption = x.IsOption,
+                    IsOptionCollection = x.IsOptionCollection,
+                    OptionType = x.OptionType,
+                    IsRequired = x.IsRequired,
+                    OptionDisplayProperty = x.OptionDisplayProperty,
+                    OptionIdProperty = x.OptionIdProperty,
+                    OptionEntityIdPropertyComposite = x.OptionEntityIdPropertyComposite,
+                    OptionRelationType = x.OptionRelationType,
+                    OptionRelationPropertyComposite = x.OptionRelationPropertyComposite,
+                    OptionRelationFirstIdProperty = x.OptionRelationFirstIdProperty,
+                    OptionRelationSecondIdProperty = x.OptionRelationSecondIdProperty,
+                }).ToList(),
+                EntityNamespace = entityInfo.Namespace,
+                MapperName = entityInfo.Name + "Mapper",
+                IsTeamType = entityInfo.BaseType?.Contains("Team") ?? false
+            };
+
+            if (string.IsNullOrWhiteSpace(dtoModel.BaseKeyType))
             {
                 consoleWriter.AddMessageLine($"WARNING: Unable to retrieve entity's base key type, you'll must replace the template '{Common.TemplateValue_BaseKeyType}' by corresponding type value inside the DTO and mapper after generation.", "orange");
-                model.BaseKeyType = Common.TemplateValue_BaseKeyType;
+                dtoModel.BaseKeyType = Common.TemplateValue_BaseKeyType;
             }
 
             consoleWriter.AddMessageLine($"Generating DTO...");
-            var dtoContent = await fileGeneratorService.GenerateFromTemplate(typeof(DtoTemplate), model);
+            var dtoContent = BiaFrameworkFileGeneratorService.GenerateFromTemplateWithScriban(dtoModel);
             var dtoDestPath = Path.Combine(
                 project.Folder,
                 Constants.FolderDotNet,
                 string.Join(".", project.CompanyName, project.Name, "Domain", "Dto"),
-                model.DomainName,
-                $"{model.DtoName}.cs");
+                dtoModel.DomainName,
+                $"{dtoModel.DtoName}.cs");
             await fileGeneratorService.GenerateFile(dtoContent, dtoDestPath);
             consoleWriter.AddMessageLine($"DTO successfully generated !", "green");
 
             consoleWriter.AddMessageLine($"Generating mapper...");
-            var mapperContent = await fileGeneratorService.GenerateFromTemplate(typeof(MapperTemplate), model);
+            var mapperContent = await fileGeneratorService.GenerateFromTemplateWithRazor(typeof(MapperTemplate), mapperModel);
             var mapperDestPath = Path.Combine(
                 Path.GetDirectoryName(entityInfo.Path),
                 "..",
                 "Mappers",
-                $"{model.MapperName}.cs");
+                $"{dtoModel.MapperName}.cs");
             await fileGeneratorService.GenerateFile(mapperContent, mapperDestPath);
             consoleWriter.AddMessageLine($"Mapper successfully generated !", "green");
         }
