de675e805bf7388c29e5963818f6b9c764698336
FOLIOT Kevin <kevin.foliot.ext@safrangroup.com>
2025-04-07 15:23:43 +0200

    Mock for entity dto team

---
+++ BIA.ToolKit.Application.TemplateGenerator/_4_0_0/Mocks/DotNet/DomainDto/EntityDtoTeamMock.cs
@@ -0,0 +1,17 @@
+﻿namespace BIA.ToolKit.Application.TemplateGenerator._4_0_0.Mocks.DotNet.DomainDto
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using BIA.ToolKit.Application.TemplateGenerator._4_0_0.Models.DotNet.DomainDto;
+
+    public class EntityDtoTeamMock : EntityDtoMock
+    {
+        public EntityDtoTeamMock() : base()
+        {
+            IsTeamType = true;
+        }
+    }
+}
