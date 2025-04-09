d70abb779421d01cf6161951feb9ea43f1cfbfa5
FOLIOT Kevin <kevin.foliot.ext@safrangroup.com>
2025-04-09 15:49:40 +0200

    Add IEntityOptionAppService template

---
+++ BIA.ToolKit.Application.Templates/_5_0_0/Templates/DotNet/Application/IEntityOptionAppService.cs
@@ -0,0 +1,16 @@
+﻿// <copyright file="IPlaneOptionAppService.cs" company="TheBIADevCompany">
+//     Copyright (c) TheBIADevCompany. All rights reserved.
+// </copyright>
+
+namespace TheBIADevCompany.BIADemo.Application.Fleet
+{
+    using BIA.Net.Core.Application.Services;
+    using BIA.Net.Core.Domain.Dto.Option;
+
+    /// <summary>
+    /// The interface defining the application service for plane option.
+    /// </summary>
+    public interface IPlaneOptionAppService : IOptionAppServiceBase<OptionDto, int>
+    {
+    }
+}
