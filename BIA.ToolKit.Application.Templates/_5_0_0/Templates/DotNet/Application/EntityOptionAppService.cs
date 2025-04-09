1b88f130c9b853ce99c40cd8a532bc12831ab264
FOLIOT Kevin <kevin.foliot.ext@safrangroup.com>
2025-04-09 15:23:07 +0200

    Adding entity option app service template

---
+++ BIA.ToolKit.Application.Templates/_5_0_0/Templates/DotNet/Application/EntityOptionAppService.cs
@@ -0,0 +1,28 @@
+﻿// <copyright file="MyEntityOptionAppService.cs" company="MyCompany">
+//     Copyright (c) MyCompany. All rights reserved.
+// </copyright>
+
+namespace MyCompany.MyProject.Application.MyDomain
+{
+    using BIA.Net.Core.Application.Services;
+    using BIA.Net.Core.Domain.Dto.Option;
+    using BIA.Net.Core.Domain.RepoContract;
+    using BIA.Net.Core.Domain.Service;
+    using MyCompany.MyProject.Domain.MyDomain.Entities;
+    using MyCompany.MyProject.MyDomain.Mappers;
+
+    /// <summary>
+    /// The application service used for myentity option.
+    /// </summary>
+    public class MyEntityOptionAppService : OptionAppServiceBase<OptionDto, MyEntity, int, MyEntityOptionMapper>, IMyEntityOptionAppService
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MyEntityOptionAppService"/> class.
+        /// </summary>
+        /// <param name="repository">The repository.</param>
+        public MyEntityOptionAppService(ITGenericRepository<MyEntity, int> repository)
+            : base(repository)
+        {
+        }
+    }
+}
