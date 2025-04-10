128d461aef8413b94496263b5258db3f11c53b73
FOLIOT Kevin <kevin.foliot.ext@safrangroup.com>
2025-04-10 14:59:47 +0200

    Add options reducer template

---
+++ BIA.ToolKit.Application.Templates/_5_0_0/Templates/Angular/src/app/domains/store/EntityOptionReducerTemplate.ts
@@ -0,0 +1,26 @@
+﻿import { EntityState, createEntityAdapter } from '@ngrx/entity';
+import { createReducer, on } from '@ngrx/store';
+import { OptionDto } from 'src/app/shared/bia-shared/model/option-dto';
+import { DomainMyCountryOptionsActions } from './my-country-options-actions';
+
+// This adapter will allow is to manipulate myCountries (mostly CRUD operations)
+export const myCountryOptionsAdapter = createEntityAdapter<OptionDto>({
+  selectId: (myCountry: OptionDto) => myCountry.id,
+  sortComparer: false,
+});
+
+export type State = EntityState<OptionDto>;
+
+export const INIT_STATE: State = myCountryOptionsAdapter.getInitialState({
+  // additional props default values here
+});
+
+export const myCountryOptionReducers = createReducer<State>(
+  INIT_STATE,
+  on(DomainMyCountryOptionsActions.loadAllSuccess, (state, { myCountries }) =>
+    myCountryOptionsAdapter.setAll(myCountries, state)
+  )
+);
+
+export const getMyCountryOptionById = (id: number) => (state: State) =>
+  state.entities[id];
