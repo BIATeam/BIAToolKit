5eba15cc1d18ecea1160a4409252c7c4730fda1b
FOLIOT Kevin <kevin.foliot.ext@safrangroup.com>
2025-04-14 15:25:48 +0200

    Add partial crud rights template

---
+++ BIA.ToolKit.Application.Templates/_5_0_0/Templates/DotNet/CrosscuttingCommon/PartialCrudRightsTemplate.cs
@@ -0,0 +1,39 @@
+﻿        // BIAToolKit - Begin Partial Rights Plane
+
+        /// <summary>
+        /// The Planes rights.
+        /// </summary>
+        public static class Planes
+        {
+            /// <summary>
+            /// The right to access to the list of Planes.
+            /// </summary>
+            public const string ListAccess = "Plane_List_Access";
+
+            /// <summary>
+            /// The right to create Planes.
+            /// </summary>
+            public const string Create = "Plane_Create";
+
+            /// <summary>
+            /// The right to read Planes.
+            /// </summary>
+            public const string Read = "Plane_Read";
+
+            /// <summary>
+            /// The right to update Planes.
+            /// </summary>
+            public const string Update = "Plane_Update";
+
+            /// <summary>
+            /// The right to delete Planes.
+            /// </summary>
+            public const string Delete = "Plane_Delete";
+
+            /// <summary>
+            /// The right to save Planes.
+            /// </summary>
+            public const string Save = "Plane_Save";
+        }
+
+        // BIAToolKit - End Partial Rights Plane
