b0756ca71abd7e164d33f3212a89ff679eef4876
FOLIOT Kevin <kevin.foliot.ext@safrangroup.com>
2025-04-18 11:28:14 +0200

    Add angular entity store files to templates

---
+++ BIA.ToolKit.Application.Templates/_5_0_0/Templates/Angular/src/app/features/store/EntityReducerTemplate.ts
@@ -0,0 +1,60 @@
+﻿import { createEntityAdapter, EntityState } from '@ngrx/entity';
+import { createReducer, on } from '@ngrx/store';
+import {
+  CrudState,
+  DEFAULT_CRUD_STATE,
+} from 'src/app/shared/bia-shared/model/crud-state';
+import { MaintenanceTeam } from '../model/maintenance-team';
+import { FeatureMaintenanceTeamsActions } from './maintenance-teams-actions';
+
+// This adapter will allow is to manipulate maintenanceTeams (mostly CRUD operations)
+export const maintenanceTeamsAdapter = createEntityAdapter<MaintenanceTeam>({
+  selectId: (maintenanceTeam: MaintenanceTeam) => maintenanceTeam.id,
+  sortComparer: false,
+});
+
+export interface State extends CrudState<MaintenanceTeam>, EntityState<MaintenanceTeam> {
+  // additional props here
+}
+
+export const INIT_STATE: State = maintenanceTeamsAdapter.getInitialState({
+  ...DEFAULT_CRUD_STATE(),
+  // additional props default values here
+});
+
+export const maintenanceTeamReducers = createReducer<State>(
+  INIT_STATE,
+  on(FeatureMaintenanceTeamsActions.clearAll, state => {
+    const stateUpdated = maintenanceTeamsAdapter.removeAll(state);
+    stateUpdated.totalCount = 0;
+    return stateUpdated;
+  }),
+  on(FeatureMaintenanceTeamsActions.clearCurrent, state => {
+    return { ...state, currentItem: <MaintenanceTeam>{} };
+  }),
+  on(FeatureMaintenanceTeamsActions.loadAllByPost, state => {
+    return { ...state, loadingGetAll: true };
+  }),
+  on(FeatureMaintenanceTeamsActions.load, state => {
+    return { ...state, loadingGet: true };
+  }),
+  on(FeatureMaintenanceTeamsActions.save, state => {
+    return { ...state, loadingGetAll: true };
+  }),
+  on(FeatureMaintenanceTeamsActions.loadAllByPostSuccess, (state, { result, event }) => {
+    const stateUpdated = maintenanceTeamsAdapter.setAll(result.data, state);
+    stateUpdated.totalCount = result.totalCount;
+    stateUpdated.lastLazyLoadEvent = event;
+    stateUpdated.loadingGetAll = false;
+    return stateUpdated;
+  }),
+  on(FeatureMaintenanceTeamsActions.loadSuccess, (state, { maintenanceTeam }) => {
+    return { ...state, currentItem: maintenanceTeam, loadingGet: false };
+  }),
+  on(FeatureMaintenanceTeamsActions.failure, state => {
+    return { ...state, loadingGetAll: false, loadingGet: false };
+  })
+);
+
+export const getMaintenanceTeamById = (id: number) => (state: State) =>
+  state.entities[id];
