bebc002b26e6022a76ab2f69c0b96bbf2e890276
FOLIOT Kevin <kevin.foliot.ext@safrangroup.com>
2025-05-20 15:35:01 +0200

    Fix entity edit component template

--- BIA.ToolKit.Application.Templates/_5_0_0/Templates/Angular/src/app/features/views/edit/EntityEditComponentTsTemplate.ts
+++ BIA.ToolKit.Application.Templates/_5_0_0/Templates/Angular/src/app/features/views/edit/EntityEditComponentTsTemplate.ts
@@ -1,4 +1,4 @@
-﻿import { Component, Injector } from '@angular/core';
+﻿import { Component, Injector, OnInit } from '@angular/core';
 import { SpinnerComponent } from 'src/app/shared/bia-shared/components/spinner/spinner.component';
 import { CrudItemEditComponent } from 'src/app/shared/bia-shared/feature-templates/crud-items/views/crud-item-edit/crud-item-edit.component';
 import { AsyncPipe, NgIf } from '@angular/common';
@@ -6,18 +6,48 @@ import { MaintenanceTeamFormComponent } from '../../components/maintenance-team-
 import { MaintenanceTeam } from '../../model/maintenance-team';
 import { maintenanceTeamCRUDConfiguration } from '../../maintenance-team.constants';
 import { MaintenanceTeamService } from '../../services/maintenance-team.service';
+import { filter } from 'rxjs';
+import { FormReadOnlyMode } from 'src/app/shared/bia-shared/feature-templates/crud-items/model/crud-config';
+import { MaintenanceTeamOptionsService } from '../../services/maintenance-team-options.service';
 
 @Component({
   selector: 'app-maintenance-team-edit',
   templateUrl: './maintenance-team-edit.component.html',
   imports: [NgIf, MaintenanceTeamFormComponent, AsyncPipe, SpinnerComponent],
 })
-export class MaintenanceTeamEditComponent extends CrudItemEditComponent<MaintenanceTeam> {
+export class MaintenanceTeamEditComponent extends CrudItemEditComponent<MaintenanceTeam> implements OnInit {
   constructor(
     protected injector: Injector,
+    protected maintenanceTeamOptionsService: MaintenanceTeamOptionsService,
     public maintenanceTeamService: MaintenanceTeamService
   ) {
     super(injector, maintenanceTeamService);
     this.crudConfiguration = maintenanceTeamCRUDConfiguration;
+  }
+
+  ngOnInit(): void {
+    super.ngOnInit();
+    this.sub.add(
+      this.biaTranslationService.currentCulture$.subscribe(() => {
+        this.maintenanceTeamOptionsService.loadAllOptions();
+      })
+    );
+  }
+
+  protected setPermissions(): void {
+    super.setPermissions();
+
+    this.permissionSub.add(
+      this.crudItemService.crudItem$
+        .pipe(filter(maintenanceTeam => !!maintenanceTeam && Object.keys(maintenanceTeam).length > 0))
+        .subscribe(maintenanceTeam => {
+          if (
+            this.crudConfiguration.isFixable === true &&
+            maintenanceTeam.isFixed === true
+          ) {
+            this.formReadOnlyMode = FormReadOnlyMode.on;
+          }
+        })
+    );
   }
 }
