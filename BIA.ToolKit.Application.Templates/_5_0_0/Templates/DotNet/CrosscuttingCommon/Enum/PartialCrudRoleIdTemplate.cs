2d1dfbb6bdee7458ed5e42246d20f9462d795927
FOLIOT Kevin <kevin.foliot.ext@safrangroup.com>
2025-05-28 11:57:26 +0200

    Fix partial templates

--- BIA.ToolKit.Application.Templates/_5_0_0/Templates/DotNet/CrosscuttingCommon/Enum/PartialCrudRoleIdTemplate.cs
+++ BIA.ToolKit.Application.Templates/_5_0_0/Templates/DotNet/CrosscuttingCommon/Enum/PartialCrudRoleIdTemplate.cs
@@ -6,9 +6,4 @@
         // TODO after creation of team MaintenanceTeam : adapt the enum value
         MaintenanceTeamAdmin = 3,
 
-        /// <summary>
-        /// The AircraftMaintenanceCompanyTeamLeader role identifier
-        /// </summary>
-        // TODO after creation of team MaintenanceTeam : adapt the enum value
-        AircraftMaintenanceCompanyTeamLeader = 201,
         // BIAToolKit - End Partial RoleId MaintenanceTeam
--- BIA.ToolKit.Application.Templates/_5_0_0/Templates/DotNet/InfrastructureData/ModelBuilders/PartialCrudUserModelBuilderTemplate_TeamTypeRoleModelBuilder.cs
+++ BIA.ToolKit.Application.Templates/_5_0_0/Templates/DotNet/InfrastructureData/ModelBuilders/PartialCrudUserModelBuilderTemplate_TeamTypeRoleModelBuilder.cs
@@ -1,5 +1,4 @@
 ﻿                    // BIAToolKit - Begin Partial TeamTypeRoleModelBuilder MaintenanceTeam
                     rt.HasData(new { TeamTypesId = (int)TeamTypeId.MaintenanceTeam, RolesId = (int)RoleId.MaintenanceTeamAdmin });
-                    rt.HasData(new { TeamTypesId = (int)TeamTypeId.MaintenanceTeam, RolesId = (int)RoleId.AircraftMaintenanceCompanyTeamLeader });
 
                     // BIAToolKit - End Partial TeamTypeRoleModelBuilder MaintenanceTeam
