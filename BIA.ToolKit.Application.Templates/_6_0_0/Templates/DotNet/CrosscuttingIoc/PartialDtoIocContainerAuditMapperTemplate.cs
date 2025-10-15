b6a5ee8c5b3b68fe7b83ca9226e4dee0b7fd2d7a
FOLIOT Kevin <kevin.foliot.ext@safrangroup.com>
2025-10-15 15:02:44 +0200

    Add partial dto ioc container template

---
+++ BIA.ToolKit.Application.Templates/_6_0_0/Templates/DotNet/CrosscuttingIoc/PartialDtoIocContainerAuditMapperTemplate.cs
@@ -0,0 +1,4 @@
+﻿            // BIAToolKit - Begin Partial IocAuditMapper Plane
+            collection.AddSingleton<IAuditMapper, PlaneAuditMapper>();
+
+            // BIAToolKit - End Partial IocAuditMapper Plane
