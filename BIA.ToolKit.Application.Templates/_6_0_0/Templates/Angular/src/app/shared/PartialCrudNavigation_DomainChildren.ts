c5f118ca3c65d03d107779e702993a446cfdfc5a
FOLIOT Kevin <kevin.foliot.ext@safrangroup.com>
2025-10-23 11:32:40 +0200

    Update template

--- BIA.ToolKit.Application.Templates/_6_0_0/Templates/Angular/src/app/shared/PartialCrudNavigation_DomainChildren.ts
+++ BIA.ToolKit.Application.Templates/_6_0_0/Templates/Angular/src/app/shared/PartialCrudNavigation_DomainChildren.ts
@@ -3,6 +3,5 @@
         labelKey: 'app.planes',
         permissions: [Permission.Plane_List_Access],
         path: ['/planes'],
-        icon: 'pi pi-th-large',
       },
       // BIAToolKit - End Partial NavigationDomainFleetChildren Plane
