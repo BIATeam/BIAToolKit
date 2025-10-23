08f8216a4439a2a2b301655c719829a1936c54f7
FOLIOT Kevin <kevin.foliot.ext@safrangroup.com>
2025-10-23 09:12:55 +0200

    Add new partial generation file for routing domain

---
+++ BIA.ToolKit.Application.Templates/_6_0_0/Templates/Angular/src/app/PartialCrudAppRoutingModule_Domain.ts
@@ -0,0 +1,13 @@
+﻿          // BIAToolKit - Begin Partial RoutingDomain Fleet
+          {
+            path: 'fleet',
+            data: {
+              breadcrumb: 'app.fleet',
+              canNavigate: false,
+            },
+            children: [
+              // BIAToolKit - Begin Routing Fleet
+              // BIAToolKit - End Routing Fleet
+            ],
+          },
+          // BIAToolKit - End Partial RoutingDomain Fleet
