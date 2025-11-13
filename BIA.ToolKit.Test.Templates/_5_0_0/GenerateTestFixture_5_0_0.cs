a5f247299f8cf24b2d74f76cd51ff6891f8041a4
FOLIOT Kevin <kevin.foliot.ext@safrangroup.com>
2025-11-13 12:54:31 +0100

    Use BIADemo 5.2.1 for UT of templates V5

--- BIA.ToolKit.Test.Templates/_5_0_0/GenerateTestFixture_5_0_0.cs
+++ BIA.ToolKit.Test.Templates/_5_0_0/GenerateTestFixture_5_0_0.cs
@@ -7,12 +7,12 @@
     {
         public GenerateTestFixture_5_0_0()
         {
-            Init("BIADemo_5.2.0", new Project
+            Init("BIADemo_5.2.1", new Project
             {
                 Name = "BIADemo",
                 CompanyName = "TheBIADevCompany",
                 BIAFronts = ["Angular"],
-                FrameworkVersion = "5.2.0"
+                FrameworkVersion = "5.2.1"
             });
         }
     }
