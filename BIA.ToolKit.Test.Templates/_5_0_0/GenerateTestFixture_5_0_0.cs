7bc88dd757adb8338a2ec21e4412c73a31936576
FOLIOT Kevin <kevin.foliot.ext@safrangroup.com>
2025-12-16 08:55:35 +0100

    Update test fixture for V5

--- BIA.ToolKit.Test.Templates/_5_0_0/GenerateTestFixture_5_0_0.cs
+++ BIA.ToolKit.Test.Templates/_5_0_0/GenerateTestFixture_5_0_0.cs
@@ -7,12 +7,12 @@
     {
         public GenerateTestFixture_5_0_0()
         {
-            Init("BIADemo_5.2.1", new Project
+            Init("BIADemo_5.2.2", new Project
             {
                 Name = "BIADemo",
                 CompanyName = "TheBIADevCompany",
                 BIAFronts = ["Angular"],
-                FrameworkVersion = "5.2.1"
+                FrameworkVersion = "5.2.2"
             });
         }
     }
