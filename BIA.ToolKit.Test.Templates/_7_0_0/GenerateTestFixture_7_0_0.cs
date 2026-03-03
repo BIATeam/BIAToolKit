fa7dcbd8a900d2aa329eeea13ddb03f47ec71195
FOLIOT Kevin <kevin.foliot.ext@safrangroup.com>
2026-03-03 09:06:42 +0100

    Add tests for V7 and update target version for V6 tests to 6.1.1

--- BIA.ToolKit.Test.Templates/_7_0_0/GenerateTestFixture_7_0_0.cs
+++ BIA.ToolKit.Test.Templates/_7_0_0/GenerateTestFixture_7_0_0.cs
@@ -1,18 +1,18 @@
-﻿namespace BIA.ToolKit.Test.Templates._6_0_0
+﻿namespace BIA.ToolKit.Test.Templates._7_0_0
 {
     using BIA.ToolKit.Domain.ModifyProject;
     using BIA.ToolKit.Test.Templates;
 
-    public sealed class GenerateTestFixture_6_0_0 : GenerateTestFixture
+    public sealed class GenerateTestFixture_7_0_0 : GenerateTestFixture
     {
-        public GenerateTestFixture_6_0_0()
+        public GenerateTestFixture_7_0_0()
         {
-            Init("BIADemo_6.1.1", new Project
+            Init("BIADemo_7.0.0", new Project
             {
                 Name = "BIADemo",
                 CompanyName = "TheBIADevCompany",
                 BIAFronts = ["Angular"],
-                FrameworkVersion = "6.1.1"
+                FrameworkVersion = "7.0.0"
             });
         }
     }
